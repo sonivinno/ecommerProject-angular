fc3bfba68e9b31256cd39003ada4e7018f1ba8ef
Soni <soni.vinno@gmail.com>
2022-05-26 18:43:42 +0530

    Added comma

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -8,7 +8,7 @@ import { AuthService } from './service/auth.service';
 })
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService,
-    private router: Router) {}
+    private router: Router,) {}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
